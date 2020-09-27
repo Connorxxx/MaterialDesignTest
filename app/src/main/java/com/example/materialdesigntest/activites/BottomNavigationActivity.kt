d9d19c7d652132826d27a9475440964dd2c4237e
WuYingHao <wyh2542367414d@gmail.com>
2020-09-27 22:33:32 +0800

    add BottomNavigationActivity

--- app/src/main/java/com/example/materialdesigntest/activites/BottomNavigationActivity.kt
+++ app/src/main/java/com/example/materialdesigntest/activites/BottomNavigationActivity.kt
@@ -1,14 +1,16 @@
 package com.example.materialdesigntest.activites
 
+import android.animation.ArgbEvaluator
 import android.os.Bundle
 import android.view.View
+import androidx.viewpager2.widget.ViewPager2
 import com.example.materialdesigntest.R
 import com.example.materialdesigntest.apadter.ViewPagerAdapter
 import kotlinx.android.synthetic.main.activity_bottom_navigation.*
-import kotlinx.android.synthetic.main.item_view_pager.*
-
 
 class BottomNavigationActivity : BaseActivity() {
+    lateinit var viewPager2: ViewPager2
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_bottom_navigation)
@@ -19,15 +21,66 @@ class BottomNavigationActivity : BaseActivity() {
         setStatusBarColor(getColor(R.color.transparent))
         setToolbar(toolbar2)
 
-        val view1 :View = layoutInflater.inflate(R.layout.item_view_pager, null)
-        val view2 :View = layoutInflater.inflate(R.layout.item_view_pager, null)
-        val view3 :View = layoutInflater.inflate(R.layout.item_view_pager, null)
-        val view4 :View = layoutInflater.inflate(R.layout.item_view_pager, null)
+        viewPager2 = findViewById(R.id.viewPagerBottomNavigation)
+        val view1: View = layoutInflater.inflate(R.layout.item_view_pager, null)
+        val view2: View = layoutInflater.inflate(R.layout.item_view_pager, null)
+        val view3: View = layoutInflater.inflate(R.layout.item_view_pager, null)
+        val view4: View = layoutInflater.inflate(R.layout.item_view_pager, null)
         val viewList: MutableList<View> = ArrayList()
         viewList.add(view1)
         viewList.add(view2)
         viewList.add(view3)
         viewList.add(view4)
-        viewPager2.adapter = ViewPagerAdapter(viewList)
+        viewPager2.adapter = ViewPagerAdapter(viewList, this)
+
+        viewPager2.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+            override fun onPageScrolled(
+                position: Int,
+                positionOffset: Float,
+                positionOffsetPixels: Int
+            ) {
+                val evaluator = ArgbEvaluator()
+                var evaluate = resources.getColor(R.color.app_blue)
+                when (position) {
+                    0 -> evaluate = evaluator.evaluate(
+                        positionOffset,
+                        resources.getColor(R.color.app_blue),
+                        resources.getColor(R.color.app_green)
+                    ) as Int
+                    1 -> evaluate = evaluator.evaluate(
+                        positionOffset,
+                        resources.getColor(R.color.app_green),
+                        resources.getColor(R.color.app_yellow)
+                    ) as Int
+                    2 -> evaluate = evaluator.evaluate(
+                        positionOffset,
+                        resources.getColor(R.color.app_yellow),
+                        resources.getColor(R.color.app_red)
+                    ) as Int
+                    else -> evaluate = resources.getColor(R.color.app_red)
+                }
+                (viewPager2.parent as View).setBackgroundColor(evaluate)
+            }
+
+            override fun onPageSelected(position: Int) {
+                when (position) {
+                    0 -> bottomNavigationView.selectedItemId = R.id.menuBlue
+                    1 -> bottomNavigationView.selectedItemId = R.id.menuGreen
+                    2 -> bottomNavigationView.selectedItemId = R.id.menuYellow
+                    3 -> bottomNavigationView.selectedItemId = R.id.menuRed
+                }
+            }
+        })
+
+
+        bottomNavigationView.setOnNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.menuBlue -> viewPager2.currentItem = 0
+                R.id.menuGreen -> viewPager2.currentItem = 1
+                R.id.menuYellow -> viewPager2.currentItem = 2
+                R.id.menuRed -> viewPager2.currentItem = 3
+            }
+            true
+        }
     }
 }
--- app/src/main/java/com/example/materialdesigntest/apadter/ViewPagerAdapter.kt
+++ app/src/main/java/com/example/materialdesigntest/apadter/ViewPagerAdapter.kt
@@ -1,22 +1,36 @@
 package com.example.materialdesigntest.apadter
 
+import android.content.Context
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.coordinatorlayout.widget.CoordinatorLayout
 import androidx.recyclerview.widget.RecyclerView
+import com.example.materialdesigntest.R
 
-class ViewPagerAdapter(private val viewList: List<View>) :
+
+class ViewPagerAdapter(private val viewList: List<View>, val context: Context) :
     RecyclerView.Adapter<ViewPagerAdapter.ViewHolder>() {
 
-    inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+    private val colorArray = intArrayOf(
+        R.color.google_blue,
+        R.color.google_green,
+        R.color.google_yellow,
+        R.color.google_red
+    )
 
+    inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val coordinatorLayout: CoordinatorLayout = view.findViewById(R.id.container)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        TODO("Not yet implemented")
+        val view = LayoutInflater.from(context)
+            .inflate(R.layout.item_view_pager, parent, false)
+        return ViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        //holder.coordinatorLayout.setBackgroundResource(colorArray[position])
     }
 
     override fun getItemCount() = viewList.size
